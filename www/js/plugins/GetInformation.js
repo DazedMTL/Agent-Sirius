7ad013fd45df5b4bb9c47178d9e3395e0791fcea
Dazed <dazedanon@gmail.com>
2024-01-29 14:53:51 -0600

    Fix 'Registered' Popup

--- www/js/plugins/GetInformation.js
+++ www/js/plugins/GetInformation.js
@@ -426,7 +426,7 @@ if (!Imported.CommonPopupCore) {
   Game_Interpreter.prototype.pluginCommand = function (command, args) {
     _gInfo_GInterpreter_pluginCommand.call(this, command, args);
     if (command === "ShowInfo" || command === "インフォ表示") {
-      CommonPopupManager.showInfo({}, args[0], null);
+      CommonPopupManager.showInfo({}, '\\C[3]Registered\\C[0] ' + args[0].replace(/_/g, ' '), null);
     }
   };
 
